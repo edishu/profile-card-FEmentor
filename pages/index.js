fd5e3c7209d3e057c0d13839a235f90bd9502a4d
Udayan Maurya <edmauryaishu@gmail.com>
2021-06-17 22:56:38 -0400

    WIP

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,37 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div>
+    <div className="min-h-screen min-w-screen bg-pattern-top flex justify-center items-center">
       <Head>
         <title>Frontend Mentor | Profile card component</title>
         <link rel="icon" href="/favicon-32x32.png" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
-      Victor Crest 26 London 80K Followers 803K Likes 1.4K Photos
-      <div>
-        Challenge by{" "}
-        <a
-          href="https://www.frontendmentor.io?ref=challenge"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Frontend Mentor
-        </a>
-        Coded by me.
+
+      <div className="w-1/4 h-96 bg-gray-100 rounded-xl">
+        <div className="h-4/5 flex flex-col justify-center items-center">
+          <img className="rounded-full" src="/image-victor.jpg" alt="Victor" />
+          <div className="mt-8 text-center">
+            <span className="text-lg font-bold mr-2">Victor Crest</span>
+            <span className="text-lg font-normal text-gray-500">26</span>
+            <div>London</div>
+          </div>
+        </div>
+
+        <div>
+          <span>
+            <div>80K</div> <div>Followers</div>
+          </span>
+          <span>
+            <div>803K</div> <div>Likes</div>
+          </span>
+          <span>
+            <div>1.4K </div> <div>Photos</div>
+          </span>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Kumbh Sans", "ui-sans-serif", "system-ui"],
+      serif: ["ui-serif", "Georgia"],
+      mono: ["ui-monospace", "SFMono-Regular"]
+    },
+    extend: {
+      backgroundImage: () => ({
+        "pattern-top": "url('/bg-pattern-top.svg')",
+        "pattern-bottom": "url('/bg-pattern-bottom.svg')",
+        "pattern-card": "url('/bg-pattern-card.svg')"
+      })
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
